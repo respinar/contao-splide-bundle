3a6cf44e6e6f8f57d555bb2cfcf4c56ff5a83fc3
Hamid A. Peywasti <abbaszadeh.h@gmail.com>
2023-08-15 22:02:25 +0330

    add dependecncy injection

---
+++ src/DependencyInjection/RespinarSplideExtension.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Splide.
+ *
+ * (c) Hamid Peywasti 2023 <abbaszadeh.h@gmail.com>
+ * @license MIT
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/respinar/contao-splide
+ */
+
+namespace Respinar\SplideBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class RespinarSplideExtension extends Extension
+{
+    /**
+     * @throws \Exception
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../../config')
+        );
+
+        $loader->load('services.yaml');
+    }
+}
