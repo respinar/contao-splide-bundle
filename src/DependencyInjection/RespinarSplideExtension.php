ddf824542660c95a472493ab53c55bd81a864d97
Hamid A. Peywasti <abbaszadeh.h@gmail.com>
2023-08-16 22:57:05 +0330

    cleanup

--- src/DependencyInjection/RespinarSplideExtension.php
+++ src/DependencyInjection/RespinarSplideExtension.php
@@ -6,10 +6,8 @@ declare(strict_types=1);
  * This file is part of Contao Splide.
  *
  * (c) Hamid Peywasti 2023 <abbaszadeh.h@gmail.com>
+ * 
  * @license MIT
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/respinar/contao-splide
  */
 
 namespace Respinar\SplideBundle\DependencyInjection;
