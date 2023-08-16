0b36c3946c239a86e150c20b20a53665920fad50
Hamid A. Peywasti <abbaszadeh.h@gmail.com>
2023-08-16 22:59:00 +0330

    Splide Model

---
+++ src/Model/SplideModel.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Splide.
+ *
+ * (c) Hamid Peywasti 2023 <abbaszadeh.h@gmail.com>
+ * 
+ * @license MIT
+ */
+
+namespace Respinar\SplideBundle\Model;
+
+use Contao\Model;
+
+class SplideModel extends Model
+{
+    protected static $strTable = 'tl_splide';
+}
