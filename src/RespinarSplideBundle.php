59985386338703730dd0c2002bae5f600601d5b0
Hamid A. Peywasti <abbaszadeh.h@gmail.com>
2023-08-20 23:35:46 +0330

    add functionalty

--- src/RespinarSplideBundle.php
+++ src/RespinarSplideBundle.php
@@ -6,14 +6,27 @@ declare(strict_types=1);
  * This file is part of Contao Splide.
  *
  * (c) Hamid Peywasti 2023 <abbaszadeh.h@gmail.com>
- * 
+ *
  * @license MIT
  */
 
 namespace Respinar\SplideBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class RespinarSplideBundle extends Bundle
 {
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+    }
 }
