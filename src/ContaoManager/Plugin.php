53bc6616e4f932bbf8ccee8c2b9f606e4010713d
Hamid Peywasti <hamid@respinar.com>
2024-02-16 20:13:20 +0330

    Update License year and email

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of Contao Splide.
  *
- * (c) Hamid Peywasti 2023 <abbaszadeh.h@gmail.com>
+ * (c) Hamid Peywasti 2024 <hamid@respinar.com>
  * 
  * @license MIT
  */
